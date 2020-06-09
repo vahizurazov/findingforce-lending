181c47dcfd127a01cce539c5586ca349a2f77c37
AndriiSchastlivcev <dj_winston@outlook.com>
2020-06-09 16:54:30 +0300

    Pull (#17)
    * FF-165 add favicon / edit configs
    
    * FF-168 add seo
    
    * change files structure
    add static meta data
    
    * add script for generate new markdown posts
    
    * FF-178 add netlify CMS
    
    * FF-178 add netlify CMS
    
    * FF-178 remove SEO config
    
    * FF-166 add page with iframe (#16)
    
    FF-166 add page with iframe
    
    Co-authored-by: Andrew Schastlivcev <andrew.schastlivcev@sparkybit.com>

---
+++ src/pages/Introduction.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="external-app">
+    <iframe
+      src="http://findingforce-test.westeurope.cloudapp.azure.com/introduction"
+      style="position: absolute; height: 100%; border: none"
+      height="100%"
+      width="100%"
+      scrolling="no"
+      frameborder="0"
+    ></iframe>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Introduction',
+  metaInfo: {
+    title: 'Introduction',
+    meta: [{ name: 'resource-type', content: 'Finding Force Application' }]
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+iframe {
+  position: absolute;
+  height: 100%;
+}
+</style>
